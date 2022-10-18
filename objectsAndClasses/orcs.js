454b2aa3b9e4eada3fb0704a33a585523e2bdb75
Anjalant Nelson <nelson.anjalant4@gmail.com>
2022-10-18 14:48:42 -0600

    created class character

--- objectsAndClasses/orcs.js
+++ objectsAndClasses/orcs.js
@@ -1,29 +1,139 @@
-/* class Vehicle {
-    //constructor
-    constructor(vin, make) {
-        this.vin = vin;
-        this.make = make;
-    }
+// const adventurer = {
+//     name: 'Aj',
+//     hitpoints: 10,
+//     belongings: ["sword", 'potion', 'food'],
+//     companion: {
+//         name: 'terra'
+//         type: 'mage'
+//         companion: {
+//             name: 'klaus'
+//             type:'wolf'
+//         }
+//     }
+// }
+
+//console.log(adventurer.belongings[0])
+
+/* for(let i = 0; i < adventurer.belongings.length; i++); {
+    console.log(adventurer.belongings[i])
 }
+*/
+/*acess the companion object
+console.log(adventurer.companion) // { }
+console.log(adventurer.companion.name) // values inside { }
+console.log(adventurer.companion.type)
 
 
-const plane = new Vehicle('a;lkfjdkljfklajskdfjdklsjflk;j', 'Boeing');
+console.log(adventurer.companion.companion.type);
 
-const myFirstCar = new Vehicle('ac;mdijiowerij', 'Honda');
+//get health insurance
+console.log(adventurer.companion.companion.belongings[2])
+*/
+// 
+/* const movies = [
+    { title: 'toy story' },
+    { title: 'wild wild west' },
+    { title: 'the purge' },
+]
 
-console.log(plane);
+console.log(movies) //array of objects
+console.log(movies[0]) //first object in the array
+console.log(movies[0.title]) 
+
+
+for(let i = 0; i < movies.length i++);{
+    console.log(movies[i])
+    console.log(movies[i].title);
+}
 */
 
-//static method
-Start(); {
-    this.running = true;
-console.log('running')
-}
+// const foo = {
+//     someArray: [1, 2, 3,]
+//     someObject: {
+//         someProp: 'heres johnny'
+//     }
+//     someMethod: function () => {
+//         console.log('inside of method')
+//     }
 
-//instance method
+// }
 
+// console.log(foo.someObject.someProp); // heres johnny
+// console.log(foo.someArray{0} // 1
+// console.log(foo.someMethod())
 
-//instantiated new object
-const car = new Vehicle('12929', 'Toyota', 'Prius');
+// const foo = [
+//     [1, 2, 3, 4], //0
+//     [5, 6, 7, 8], //1
+//     [9, 10, 11, 12], //2
+// ]
 
-car.start()
+// cosnole.log(foo[1][2]); //7
+
+// const foo = [
+//     1,
+//     'hi',
+//     ()=>{
+//         console.log('fun')
+//     }
+// ]
+
+// console.log(foo[2]() );
+
+// const player = {
+//     name: 'king arthur',
+//     health: 1000,
+//     power: 9000,
+//     stamina: 5000,
+// }
+
+// const createEnemy = (nameIs, healthIs, powerIs, staminaIs) => {
+//     const newEnemy = {
+//         name: nameIs
+//         health: healthIs
+//         power: powerIs
+//         stamina: staminaIs
+//     }
+//     return newEnemy
+// };
+
+// const createPlayer = (nameIs, healthIs, powerIs, staminaIs) => {
+//     const newPlayer = {
+//         name: nameIs
+//         health: healthIs
+//         power: powerIs
+//         stamina; staminaIs
+//     }
+//     return newPlayer;
+// }
+
+// class Character {
+    //constructor
+    // constructer(name, age, eyes, hair, lovesCats = false, lovesDogs = true) {
+    //     this.legs = 2;
+    //     this.arms = 2;
+    //     this.eyes: eyes;
+    //     this.hair: hair
+//             this.lovesCats = lovesCats
+//             this.lovesDogs = lovesDogs            
+//  }
+    // greet(otherCharacter) {
+    //     console.log('hi ' + otherCharacter + '!');
+    // }
+        // smite() {
+        //     console.log('I smite thee you vile person');
+        // }
+// }
+
+// const p1 = new Character();
+// const enemy = new Character();
+
+// console.log(p1, enemy)
+
+// console.log(p1.greet('bob'))
+// console.log(p1.smite())
+
+// console.log(enemy.greet('sauron'))
+
+// console.log(p1)
+
